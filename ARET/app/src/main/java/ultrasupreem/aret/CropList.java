ab365fcc0adffd8dad6939e13906edc96ff40a19
Ethan <evanhout@uoguelph.ca>
2016-11-22 21:06:30 -0500

    This should be the parse

--- ARET/app/src/main/java/ultrasupreem/aret/CropList.java
+++ ARET/app/src/main/java/ultrasupreem/aret/CropList.java
@@ -1,8 +1,7 @@
 package ultrasupreem.aret;
 
 
-import org.json.JSONObject;
-
+import org.json.*;
 import java.util.ArrayList;
 
 public class CropList {
@@ -12,7 +11,16 @@ public class CropList {
 
     }
 
-    public void parseCrops(JSONObject getResponse){
-        
+    public void parseCrops(String getResponse){ // should be a JSONObject
+        try {
+            JSONArray arr = getResponse.getJSONObject("Crop").getString("CropName");
+        }
+        catch(JSONException e) {
+            //response didnt parse so were putting in garbage data
+            for (int i =0; i<10; i++) {
+                Crop temp = new Crop(String.valueOf(i), "Crop type", "Crop Description: filler crop ","Crop instructions");
+                crops.add(temp);
+            }
+        }
     }
 }
